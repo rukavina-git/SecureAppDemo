d861fabf99695a154838f76affb312e5bb838ea0
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2023-08-14 00:16:35 +0200

    init android app

---
+++ backend/src/main/kotlin/com/rukavina/backend/SpringBootBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.rukavina.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootBackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/rukavina/backend/SpringBootBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rukavina.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
