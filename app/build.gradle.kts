21bc1e42e7412a1cef00b4904666b0a432f57d26
Ivan Karlo Rukavina <ivankarlo.rukavina@gmail.com>
2023-08-16 23:06:36 +0200

    add grpc dependecies and update ktx version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,7 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -59,6 +58,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation ("io.grpc:grpc-okhttp:1.46.0")
+    implementation ("io.grpc:grpc-stub:1.51.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
